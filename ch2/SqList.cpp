ed53749a716b167e36e638349a31e2f4a268b5ca
王晶 <wangjing@wangjingdeMacBook-Pro.local>
2020-08-06 23:54:15 +0800

    start

---
+++ ch2/SqList.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+ussing namespace std;
+#define MaxSize 50
+#define InitSize 100
+
+typedef struct SqList
+{
+    /* data */
+    int data[MaxSize];
+    int length;
+};
+
+
+bool ListInsert(SqList &L, int i)
+{
+    
+}
